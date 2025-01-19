f220d51dc2b6e7dfba42c9cb2617aec9fe5792b5
SaeedFakhrabadi <saeedfakhrabadi8@gmail.com>
2025-01-20 00:28:47 +0330

    Add in & out

--- main.py
+++ main.py
@@ -8,30 +8,6 @@ class TaskState(Enum):
     RUNNING = 3
 
 
-class Task:
-    def __init__(
-        self, name, execution_time, resources_needed, period, deadline, priority
-    ):
-        self.name = name
-        self.execution_time = execution_time
-        self.resources_needed = resources_needed
-        self.period = period
-        self.deadline = deadline
-        self.priority = priority
-        self.state = TaskState.READY
-        self.remaining_time = execution_time
-
-    def __lt__(self, other):
-        return self.priority < other.priority
-
-
-class Resource:
-    def __init__(self, name, total_units):
-        self.name = name
-        self.total_units = total_units
-        self.available_units = total_units
-
-
 class Core:
     def __init__(self, core_id):
         self.core_id = core_id
@@ -57,12 +33,46 @@ class Core:
                     self.assign_task(next_task)
 
 
+class Task:
+    def __init__(
+        self, name, execution_time, resources_needed, period, deadline, priority
+    ):
+        self.name = name
+        self.execution_time = execution_time
+        self.resources_needed = resources_needed
+        self.period = period
+        self.deadline = deadline
+        self.priority = priority
+        self.state = TaskState.READY
+        self.remaining_time = execution_time
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+
 class Subsystem:
     def __init__(self, subsystem_id, num_cores, resources):
         self.subsystem_id = subsystem_id
         self.cores = [Core(i) for i in range(num_cores)]
         self.resources = resources
         self.waiting_queue = []
+
+    def release_resources(self, task):
+        for resource, needed in task.resources_needed.items():
+            self.resources[resource].available_units += needed
+
+    def get_status(self, time):
+        # status = f"Time: {time}\n"
+        status = f"Sub{self.subsystem_id}:\n"
+        status += f"        Resources: R1: {self.resources['R1'].available_units} R2: {self.resources['R2'].available_units}\n"
+        status += (
+            f"        Waiting Queue {[task.name for task in self.waiting_queue]}\n"
+        )
+        for core in self.cores:
+            status += f"        Core{core.core_id + 1}:\n"
+            status += f"                Running Task: {core.current_task.name if core.current_task else 'idle'}\n"
+            status += f"                Ready Queue: {[task.name for task in core.ready_queue]}\n"
+        return status
 
     def allocate_resources(self, task):
         for resource, needed in task.resources_needed.items():
@@ -71,18 +81,6 @@ class Subsystem:
         for resource, needed in task.resources_needed.items():
             self.resources[resource].available_units -= needed
         return True
-
-    def release_resources(self, task):
-        for resource, needed in task.resources_needed.items():
-            self.resources[resource].available_units += needed
-
-    def add_task(self, task):
-        if self.allocate_resources(task):
-            core = self.cores[0]  # Simple core selection, can be improved
-            core.assign_task(task)
-        else:
-            self.waiting_queue.append(task)
-            task.state = TaskState.WAITING
 
     def execute(self):
         for core in self.cores:
@@ -94,6 +92,21 @@ class Subsystem:
                 core.assign_task(task)
                 self.waiting_queue.remove(task)
 
+    def add_task(self, task):
+        if self.allocate_resources(task):
+            core = self.cores[0]  # Simple core selection, can be improved
+            core.assign_task(task)
+        else:
+            self.waiting_queue.append(task)
+            task.state = TaskState.WAITING
+
+
+class Resource:
+    def __init__(self, name, total_units):
+        self.name = name
+        self.total_units = total_units
+        self.available_units = total_units
+
 
 def main():
     # Initialize resources
@@ -103,6 +116,7 @@ def main():
     subsystem1 = Subsystem(1, 3, resources)
     subsystem2 = Subsystem(2, 2, resources)
     subsystem3 = Subsystem(3, 1, resources)
+    subsystem4 = Subsystem(4, 2, resources)
 
     # Create some tasks
     tasks = [
@@ -121,15 +135,21 @@ def main():
     subsystem1.add_task(tasks[2])
     subsystem2.add_task(tasks[3])
     subsystem3.add_task(tasks[4])
-    subsystem3.add_task(tasks[5])
-    subsystem3.add_task(tasks[6])
+    subsystem4.add_task(tasks[5])
+    subsystem4.add_task(tasks[6])
 
-    # Simulate execution for 10 time units
-    for time_unit in range(10):
-        print(f"Time: {time_unit}")
-        subsystem1.execute()
-        subsystem2.execute()
-        subsystem3.execute()
+    # Simulate execution for 1 time unit
+    time_unit = 1
+    subsystem1.execute()
+    subsystem2.execute()
+    subsystem3.execute()
+    subsystem4.execute()
+
+    # Print status
+    print(subsystem1.get_status(time_unit))
+    print(subsystem2.get_status(time_unit))
+    print(subsystem3.get_status(time_unit))
+    print(subsystem4.get_status(time_unit))
 
 
 if __name__ == "__main__":
